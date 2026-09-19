30c127e5d252b8fd03ee6145f7a67bbb8ca6f7e9
Tim Schrader <holyvoltron@gmail.com>
2026-09-18 22:29:30 -0700

    clean up rendering in history tree

--- static/sw.js
+++ static/sw.js
@@ -1,5 +1,5 @@
 /* Light installable shell: cache the app chrome so a revisit still paints. */
-const CACHE = "school-nanny-shell-v14";
+const CACHE = "school-nanny-shell-v15";
 const SHELL = [
   "/static/app.css",
   "/static/pico.min.css",
